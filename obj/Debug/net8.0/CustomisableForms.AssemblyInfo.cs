b1ab3e12098861ed8b8d72c43d0e8e55fda97ba1
Kopats Daniil <kopot.dan@gmail.com>
2024-10-23 17:58:01 +0300

    temp commit

--- obj/Debug/net8.0/CustomisableForms.AssemblyInfo.cs
+++ obj/Debug/net8.0/CustomisableForms.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomisableForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d04f198a9468c7be8faa833dc6410267f9163783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+116338f690f4c70bdc8c89b35a898efaf751b3bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomisableForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomisableForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
